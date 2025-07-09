2f61e5b8c6404e8cc534686188bf699a5f1d36df
Maurizio Moraca <maurizio.moraca@capgemini.com>
2025-07-09 08:42:49 +0000

    Added AmountSummary table

--- srv/func/utils/transcoders.js
+++ srv/func/utils/transcoders.js
@@ -1,27 +1,27 @@
 const transcoder = {
-    doxStatus : {
+    doxStatus: {
         "01": "PENDING",
         "02": "DONE",
         "03": "FAILED",
         "04": "CONFIRMED"
     },
-    documentType : {
+    documentType: {
         "01": "Invoice",
         "02": "Payment Advice",
         "03": "Purchase Order"
     },
-    docCategory : {
+    docCategory: {
         "NONPOINV": "Non-PO Invoice",
         "POINV": "PO Invoice",
         "NONPOCREDM": "Non-PO Credit Memo",
         "POCREDM": "PO Credit Memo"
     },
-    paymentMethod : {
+    paymentMethod: {
         "MP05": "T",
         "MP12": "H",
         "MP19": "E"
     },
-    accountingDocumentType : {
+    accountingDocumentType: {
         "TD01": "X1",
         "TD04": "Z1",
         "TD05": "W7",
@@ -33,27 +33,35 @@ const transcoder = {
         "TD19": "X4",
         "TD20": "X4"
     },
-    taxCode : {
+    taxCode: {
+        4.00: "A4",
+        5.00: "B4",
         10.00: "A1",
         22.00: "A2",
-        0.00 : {
-            "N2.1" : "AK",
-            "N3.1" : "AO",
-            "N3.2" : "VU",
-            "N3.3" : "VS",
-            "N3.4" : "AO",
-            "N3.5" : "AZ",
-            "N3.6" : "AP",
-            "N6.1" : "AA",
-            "N6.2" : "AR",
-            "N6.3" : "V6",
-            "N6.4" : "V6",
-            "N6.5" : "V6",
-            "N6.6" : "V6",
-            "N6.7" : "V6",
-            "N6.8" : "V6",
-            "N6.9" : "V6"
+        0.00: {
+            "N2.1": "AK",
+            "N3.1": "AO",
+            "N3.2": "VU",
+            "N3.3": "VS",
+            "N3.4": "AO",
+            "N3.5": "AZ",
+            "N3.6": "AP",
+            "N6.1": "AA",
+            "N6.2": "AR",
+            "N6.3": "V6",
+            "N6.4": "V6",
+            "N6.5": "V6",
+            "N6.6": "V6",
+            "N6.7": "V6",
+            "N6.8": "V6",
+            "N6.9": "V6"
         }
+    },
+    aliquotaIVA : {
+        "A4": 0.04,
+        "B4": 0.05,
+        "A1": 0.10,
+        "A2": 0.22
     }
 }
 
